bb6a3aeba23904795e64fa1240bf99b4b2541e3b
giladby <56963755+giladby@users.noreply.github.com>
2021-03-16 14:32:56 +0200

    update food ingredients script

--- script.py
+++ script.py
@@ -4,63 +4,58 @@ import csv
 
 import mysql.connector
 
+def read_urls():
+    file = open('urls.txt', 'r')
+    return file.readlines()
 
-class MySQLConnectionFields:
-    def __init__(self, host, user, password, database):
-        self.host = host
-        self.user = user
-        self.password = password
-        self.database = database
-
-def get_db_fields():
-    host = "127.0.0.1"
-    user = "root"
-    password = "123456"
-    database = "fitasty_demo"
-    return MySQLConnectionFields(host, user, password, database)
-
-def get_mysql_connection(mysql_fields):
-    return mysql.connector.connect(
-        host=mysql_fields.host,
-        user=mysql_fields.user,
-        passwd=mysql_fields.password,
-        database=mysql_fields.database)
-
-def close_connection(conn, cursor):
-    if conn is not None:
-        conn.close()
-    if cursor is not None:
-        cursor.close()
-
-def get_mysql_cursor():
-    conn = None
-    cursor = None
+def is_float(string):
     error = False
     try:
-        db_fields = get_db_fields()
-        conn = get_mysql_connection(db_fields)
-        cursor = conn.cursor()
+        string = float(string)
     except:
-       error = True
-    return conn, cursor, error
+        error = True
+    return error, string
 
-def insert_foods_query(conn, cursor, foods_arr):
-    error = False
-    if cursor:
-        try:
-            query = f"INSERT IGNORE INTO food_ingredient (name, fat, carbs, fiber, protein) VALUES (%s, %s, %s, %s, %s)"
-            cursor.executemany(query, foods_arr)
+def get_amount(filter, string, liquid):
+    error = True
+    amount = ""
+    string = string.split(filter)[0]
+    if "(" in string:
+        parts = string.split("(")
+        error = False
+        amount = parts[len(parts) - 1]
+        error, amount = is_float(amount)
+        if liquid:
+            amount *= 29.5735296
+        else:
+            amount *= 28.3495231
+    return error, amount
 
-            conn.commit()
-        except:
-            error = True
-    return error
+def get_measure(string):
+    liquid = False
+    amount = 0
+    if " fl. oz)" in string:
+        liquid = True
+        error, amount = get_amount(" fl. oz)", string, liquid)
+    elif " oz)" not in string:
+        error = True
+    else:
+        error, amount = get_amount(" oz)", string, liquid)
+    return error, liquid, amount
+
+def convert_size(double_str, amount):
+    error, double_val = is_float(double_str)
+    if not error:
+        double_val = 100/amount * double_val
+        double_val = "{:.2f}".format(double_val)
+    return error, double_val
+
+def remove_less(string):
+    return string.split("&lt; ")[1] if "&lt; " in string else string
 
 rowsString = []
 
-urls = ["https://www.calorieking.com/us/en/foods/f/calories-in-fresh-fruits-apples-with-skin-raw/9PkqpzWLSwupYVk2vXG_pA",
-        "https://www.calorieking.com/us/en/foods/f/calories-in-sugars-granulated-white-sugar/gqBMIR0gQj6_xXTVqdaavg",
-        "https://www.calorieking.com/us/en/foods/f/calories-in-fresh-fruits-avocados-average-all-varieties-raw/zU9a9g9oQ5W3RHKaVFtG7A"]
+urls = read_urls()
 
 # Fetching the html
 for url in urls:
@@ -70,27 +65,59 @@ for url in urls:
     res = urllib.request.urlopen(req)
 
     all_str = res.read().decode("utf-8")
-    parts = all_str.split("MuiTableRow-root jss382")
+
+    type = all_str.split("<div class=\"MuiSelect-root MuiSelect-select MuiSelect-selectMenu MuiInputBase-input MuiInput-input\" "
+                         "tabindex=\"0\" role=\"button\" aria-haspopup=\"listbox\">")[2]
+    type = type.split("<")[0]
+    error, liquid, amount = get_measure(type)
+    if error:
+        continue
+
     food = all_str.split("How many calories in</span>")[1]
     food = food.split("</h1>")[0]
+
+    parts = all_str.split("MuiTableRow-root jss382")
+
     fat = parts[1].split(
         "MuiTypography-root MuiTypography-h5 MuiTypography-noWrap MuiTypography-alignCenter MuiTypography-displayBlock\">")[1]
     fat = fat.split("<")[0]
+    fat = remove_less(fat)
+    error, fat = convert_size(fat, amount)
+    if error:
+        continue
+
     carbs = parts[2].split(
         "MuiTypography-root MuiTypography-h5 MuiTypography-noWrap MuiTypography-alignCenter MuiTypography-displayBlock\">")[1]
     carbs = carbs.split("<")[0]
+    carbs = remove_less(carbs)
+    error, carbs = convert_size(carbs, amount)
+    if error:
+        continue
+
     fiber = parts[3].split(
         "MuiTypography-root MuiTypography-h5 MuiTypography-noWrap MuiTypography-alignCenter MuiTypography-displayBlock\">")[
         1]
     fiber = fiber.split("<")[0]
+    fiber = remove_less(fiber)
+    error, fiber = convert_size(fiber, amount)
+    if error:
+        continue
+
     protein = parts[4].split(
         "MuiTypography-root MuiTypography-h5 MuiTypography-noWrap MuiTypography-alignCenter MuiTypography-displayBlock\">")[
         1]
     protein = protein.split("<")[0]
-    rowsString.append((food, fat, carbs, fiber, protein))
+    protein = remove_less(protein)
+    error, protein = convert_size(protein, amount)
+    if error:
+        continue
 
-conn, cursor, error = get_mysql_cursor()
-result = None
-if not error:
-    error = insert_foods_query(conn, cursor, rowsString)
-close_connection(conn, cursor)
+    rowsString.append((food, "0" if liquid else "1", fat, carbs, fiber, protein))
+
+with open("foods.csv", mode='w', newline='', encoding='utf-8') as csv_file:
+    fieldnames = ["NAME","LIQUID", "FAT", "CARBS", "FIBER", "PROTEIN"]
+    writer = csv.DictWriter(csv_file, fieldnames=fieldnames)
+
+    writer.writeheader()
+    for row in rowsString:
+        writer.writerow({"NAME": row[0], "LIQUID": row[1], "FAT": row[2], "CARBS": row[3], "FIBER": row[4], "PROTEIN": row[5]})
